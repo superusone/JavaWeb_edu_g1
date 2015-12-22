228392c5fe1e65ed270a2d55d5973b9d69dcedbb
hanhao <ihanhao@foxmail.com>
2015-12-22 17:13:55 +0800

    paper表对应更新

---
+++ JavaWeb_edu_g1/src/cn/zjxu/exam/paper/service/PaperService.java
@@ -0,0 +1,7 @@
+package cn.zjxu.exam.paper.service;
+
+import cn.zjxu.exam.paper.dao.PaperDao;
+
+public class PaperService {
+	private PaperDao paperDao = new PaperDao();
+}
---
+++ JavaWeb_edu_g1/src/cn/zjxu/exam/paper/web/servlet/PaperServlet.java
@@ -0,0 +1,8 @@
+package cn.zjxu.exam.paper.web.servlet;
+
+import cn.itcast.servlet.BaseServlet;
+import cn.zjxu.exam.paper.service.PaperService;
+
+public class PaperServlet extends BaseServlet {
+	private PaperService paperService = new PaperService();
+}
