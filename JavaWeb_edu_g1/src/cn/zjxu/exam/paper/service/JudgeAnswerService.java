e06fdadd6587aa8b84ebd6e0991cb5ae612dee4f
tonghua <lunatic@as>
2015-12-31 15:17:33 +0800

    创建了一个JudgeAnswerService类用来判断正确答案

---
+++ JavaWeb_edu_g1/src/cn/zjxu/exam/paper/service/JudgeAnswerService.java
@@ -0,0 +1,13 @@
+package cn.zjxu.exam.paper.service;
+
+import cn.zjxu.exam.paper.dao.PaperDao;
+import cn.zjxu.exam.paper.web.servlet.PaperServlet;
+
+public class JudgeAnswerService extends PaperServlet {
+	private PaperDao paperDao = new PaperDao();
+	
+	public void Judge(String pid, String userAnswer){
+		
+	}
+
+}
