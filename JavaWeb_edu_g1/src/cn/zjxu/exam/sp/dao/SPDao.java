cc0468ff5f6a81239cd542ff96b3c2126ab41935
tonghua <lunatic@as>
2016-01-01 14:12:43 +0800

    教师改卷和查分数功能基本完成

--- JavaWeb_edu_g1/src/cn/zjxu/exam/sp/dao/SPDao.java
+++ JavaWeb_edu_g1/src/cn/zjxu/exam/sp/dao/SPDao.java
@@ -1,10 +1,14 @@
 package cn.zjxu.exam.sp.dao;
 
 import java.sql.SQLException;
+import java.util.List;
 
 import org.apache.commons.dbutils.QueryRunner;
+import org.apache.commons.dbutils.handlers.BeanHandler;
+import org.apache.commons.dbutils.handlers.BeanListHandler;
 
 import cn.itcast.jdbc.TxQueryRunner;
+import cn.zjxu.exam.paper.domain.Paper;
 import cn.zjxu.exam.sp.domain.SP;
 
 public class SPDao {
@@ -22,4 +26,16 @@ public class SPDao {
 			throw new RuntimeException(e);
 		}
 	}
+	/*
+	 * 通过id查询集合
+	 */
+	public List<SP> findById(String id) {
+		try {
+			String sql = "select *from sp where id=" + id;
+			return qr.query(sql, new BeanListHandler<SP>(SP.class));
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
+	}
+	
 }
